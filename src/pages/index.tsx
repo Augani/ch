10f832ad9e185ab45a74280ee5e4aa7c08e899a0
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2021-04-06 07:58:14 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Head from 'next/head'
+
+import CoinhavenLogo from '../assets/coinhaven.svg'
+
+import { Container } from '../styles/pages/Home'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <CoinhavenLogo />
+      <h1>NextJS + typescript</h1>
+      <p>Lorem, ipsum dolor.</p>
+    </Container>
+  )
+}
+
+export default Home
