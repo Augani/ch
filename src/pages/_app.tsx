5ea9ff7b57bdd6709484cacce2d8ca13a1c4ea09
krupalpatel92 <krupalpatel92@gmail.com>
2021-04-07 20:52:04 +0530

    finish global theme setup and configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
+import '../styles/app.css'
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
