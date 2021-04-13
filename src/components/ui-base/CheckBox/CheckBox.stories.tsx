e71b38c351c4129573461251900c215f25153b89
Augustus Otu <Augani7@gmail.com>
2021-04-13 00:12:17 +0000

    Updated stories for checkbox and tabbar

--- src/components/ui-base/CheckBox/CheckBox.stories.tsx
+++ src/components/ui-base/CheckBox/CheckBox.stories.tsx
@@ -1,50 +1,10 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React from 'react';
 import { withKnobs } from '@storybook/addon-knobs';
-import { Button } from './CheckBox';
+import { CheckBox } from './CheckBox';
 
 export default {
-  component: Button,
+  component: CheckBox,
   decorators: [withKnobs],
-  title: 'UI Base/Button'
-};
-
-export const SmallButton = () => {
-  const knobProps = {
-    size: 'small',
-    text: 'small',
-    mode: 'dark'
-  };
-
-  return <Button {...knobProps} />;
-};
-
-export const MediumButton = () => {
-  const knobProps = {
-    size: 'normal',
-    text: 'Medium',
-    mode: 'dark'
-  };
-
-  return <Button {...knobProps} />;
-};
-
-export const LargeButton = () => {
-  const knobProps = {
-    size: 'large',
-    text: 'Large',
-    mode: 'dark'
-  };
-
-  return <Button disabled={true} {...knobProps} />;
-};
-
-export const LightLargeButton = () => {
-  const knobProps = {
-    size: 'large',
-    text: 'Large',
-    mode: 'light'
-  };
-
-  return <Button {...knobProps} />;
+  title: 'UI Base/CheckBox'
 };
